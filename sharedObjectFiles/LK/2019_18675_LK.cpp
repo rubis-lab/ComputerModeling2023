d5e4eb0703f3f5ae3d19f8e8f41b6427c7935108
KJYoung <jykim157@naver.com>
2021-12-12 15:18:55 +0900

    1.10 REPORT1.md

---
+++ sharedObjectFiles/LK/2019_18675_LK.cpp
@@ -0,0 +1,48 @@
+#include "LK.h"
+
+void sim_main()
+{
+  #define STEERING_VALUE_VKJY1 10000.0 //IF.
+  #define STEERING_VALUE_VKJY2 8000.0 //Write value.
+  if (rtU->read2 <= 5000.0)
+  {
+    rtDW->w3 = 0.0;
+    rtDW->w4 = 0.0;
+  }
+  else
+  {
+    if (rtU->read1 > 0.0)
+    {
+      rtDW->w3 = 0.0;
+    }
+    else if ((rtU->read1 < 0.0) && (rtU->read1 < -STEERING_VALUE_VKJY1))
+    {
+      rtDW->w3 = STEERING_VALUE_VKJY2;
+    }
+    else
+    {
+      if ((rtU->read1 < 0.0) && (rtU->read1 >= -STEERING_VALUE_VKJY1))
+      {
+        rtDW->w3 = rtU->read1 * -1;
+      }
+    }
+
+    if ((rtU->read1 > 0.0) && (rtU->read1 > STEERING_VALUE_VKJY1))
+    {
+      rtDW->w4 = STEERING_VALUE_VKJY2;
+    }
+    else if ((rtU->read1 > 0.0) && (rtU->read1 <= STEERING_VALUE_VKJY1))
+    {
+      rtDW->w4 = rtU->read1;
+    }
+    else
+    {
+      if (rtU->read1 < 0.0)
+      {
+        rtDW->w4 = 0.0;
+      }
+    }
+  }
+  rtY->write3 = rtDW->w3;
+  rtY->write4 = rtDW->w4;
+}
