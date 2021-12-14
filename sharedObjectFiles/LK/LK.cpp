cf0a23f2fc26e3b9f514f65de8ade2278c7d209d
juimdpp <hscornelia@snu.ac.kr>
2021-12-14 13:27:31 +0900

    add build, shared object files

--- sharedObjectFiles/LK/LK.cpp
+++ sharedObjectFiles/LK/LK.cpp
@@ -15,7 +15,7 @@ void sim_main()
     }
     else if ((rtU->read1 < 0.0) && (rtU->read1 < -10000.0))
     {
-      rtDW->w3 = 5000.0;
+      rtDW->w3 = 1000.0;
     }
     else
     {
@@ -27,7 +27,7 @@ void sim_main()
 
     if ((rtU->read1 > 0.0) && (rtU->read1 > 10000.0))
     {
-      rtDW->w4 = 5000.0;
+      rtDW->w4 = 1000.0;
     }
     else if ((rtU->read1 > 0.0) && (rtU->read1 <= 10000.0))
     {
